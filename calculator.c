4218ed5c8ff5f04b713ad24867ffdf584a8ab5d5
hamza emin <hamzaemin58@gmail.com>
2026-01-30 15:34:29 +0300

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){
+
+char operator = '\0';
+double num1= 0.0;
+double num2= 0.0;
+double result= 0.0;
+
+printf("enter the first number : \n");
+scanf(" %lf" , &num1);
+ 
+printf("enter the operator ( + - * /):\n ");
+scanf(" %c", &operator);
+
+printf("enter the second number :\n");
+scanf(" %lf", &num2);
+
+switch(operator){
+
+   case '+':
+   result = num1 + num2;
+   break;
+
+   case '-':
+   result = num1 - num2;   
+   break;
+
+   case '/':
+   result = num1 / num2;
+   break;
+
+   case '*':
+   result = num1 * num2;
+   break;
+
+
+}
+
+printf("the result is %.2lf", result);
+
+    return 0;
+}
